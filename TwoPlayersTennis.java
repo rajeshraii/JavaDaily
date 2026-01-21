d64a6f2b3fa2cc17103e3782b8267d5b6a2679d9
rajesh123rai <rajes1173@gmail.com>
2026-01-21 23:17:32 +0530

    Push

--- TwoPlayersTennis.java
+++ TwoPlayersTennis.java
@@ -3,9 +3,9 @@ public class TwoPlayersTennis {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        for(int i=0;i<t;i++){
+        for(int i = 0;i < t;i++){
             int k=sc.nextInt();
-            if((k-1)%3==0){
+            if((k-1) % 3 ==0 ){
                 System.out.println("YES");
             }else{
                 System.out.println("NO");
