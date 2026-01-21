8a70e5bf92e968ce150639719c1a344a9dd5d01e
rajesh123rai <rajes1173@gmail.com>
2026-01-21 23:14:03 +0530

    New changes

---
+++ Square1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Square1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < t; i++)
+            {
+            String line = sc.nextLine();
+            String[] parts = line.split(" ");
+            int l = Integer.parseInt(parts[0]);
+            int r = Integer.parseInt(parts[1]);
+            int d = Integer.parseInt(parts[2]);
+            int u = Integer.parseInt(parts[3]);
+            if(l ==r && r ==d && d==u)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+            }
+             sc.close();
+        }
+    }
---
+++ TwoPlayersTennis.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TwoPlayersTennis {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int k=sc.nextInt();
+            if((k-1)%3==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            }
+            sc.close();
+        }
+    }
