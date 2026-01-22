f6e0486f79268752e4795f4e00544e2b7b336cbd
rajesh123rai <rajes1173@gmail.com>
2026-01-22 23:45:17 +0530

    New files

--- FalseAlaram.java
+++ FalseAlaram.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class FalseAlaram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
+
+        while (t-- > 0) {
             int n = sc.nextInt();
             int x = sc.nextInt();
-            int[] arr = new int[n];
-            for(int j=0;j<n;j++){
-                arr[j]=sc.nextInt();
-            }
-            for(int j=arr[0];j<=arr.length;j++){
-              if(arr[j]==1 && (arr[n]-arr[j])<=x){
-                    System.out.println("YES");
-                    }else{
-                        System.out.println("NO");
-                }
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
             }
 
+            int i = 0;
+
+            while (i < n && a[i] == 0) {
+                i++;
             }
-sc.close();
+            i += x;
+
+            boolean possible = true;
+            while (i < n) {
+                if (a[i] == 1) {
+                    possible = false;
+                    break;
+                }
+                i++;
+            }
+
+            System.out.println(possible ? "YES" : "NO");
+        }
+        sc.close();
     }
 }
