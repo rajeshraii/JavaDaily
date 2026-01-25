ee1095c8edb4d86ad9a501828112259560410f59
rajesh123rai <rajes1173@gmail.com>
2026-01-25 23:23:37 +0530

    commited

--- SameDifference.java
+++ SameDifference.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
+
 public class SameDifference {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int changes = 0;
         int t = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < t; i++) {
-            int n = sc.nextInt(); 
-            for(int j = 1;j <= n;j++){
+            int n = sc.nextInt();
+            for (int j = 1; j <= n; j++) {
                 String s = sc.nextLine();
                 char firstChar = s.charAt(0);
-                for(int k = 1;k < s.length();k++){
-                    if(s.charAt(k) != firstChar){
+                for (int k = 1; k < s.length(); k++) {
+                    if (s.charAt(k) != firstChar) {
                         changes++;
                     }
                 }
             }
         }
-                    System.out.println(changes);
-                    sc.close();
+        System.out.println(changes);
+        sc.close();
     }
 }
-
