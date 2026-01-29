dfa6d893a0470a2a45f7c0c5f0faeddf605c392d
rajesh123rai <rajes1173@gmail.com>
2026-01-29 23:08:32 +0530

    Initial commit

--- SoldierAndBanana.java
+++ SoldierAndBanana.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class SoldierAndBanana {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int k = scanner.nextInt();
         int n = scanner.nextInt();
         int w = scanner.nextInt();
+
         int totalCost = k * (w * (w + 1)) / 2;
+        
         int amountToBorrow = totalCost - n;
 
         if (amountToBorrow > 0) {
