8c4f19df58eff5011a236a98aad24a4c4dad7809
rajesh123rai <rajes1173@gmail.com>
2026-02-01 18:45:29 +0530

    New

--- SquareString.java
+++ SquareString.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
-
 public class SquareString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        sc.nextLine(); // consume newline
-
+        sc.nextLine();
         while (t-- > 0) {
             String s = sc.nextLine();
             int n = s.length();
-
             if (n % 2 != 0) {
                 System.out.println("NO");
             } else {
