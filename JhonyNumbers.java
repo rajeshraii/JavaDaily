a7986316589b0c4a444a6fb516e233c9a3fbf7f8
rajesh123rai <rajes1173@gmail.com>
2026-02-02 20:18:29 +0530

    New

--- JhonyNumbers.java
+++ JhonyNumbers.java
@@ -2,9 +2,11 @@ import java.util.*;
 public class JhonyNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int k = sc.nextInt();
         int x = (n/k +1)*k;
+        
         System.out.println(x);
         sc.close();
 }
