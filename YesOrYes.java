c22fa66eca5107ee2ad5cd5ba4b7dc00a96a7ceb
rajesh123rai <rajes1173@gmail.com>
2026-02-02 20:19:17 +0530

    New

--- YesOrYes.java
+++ YesOrYes.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class YesOrYes{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
         sc.nextLine();
+
         for(int i = 0;i< t;i++){
         String s = sc.nextLine();
+        
         if(s.equalsIgnoreCase("yes")){
             System.out.println("Yes");
         }
