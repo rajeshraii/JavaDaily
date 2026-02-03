dabfbe885025caf2f73850bd9d5310e3c76cf987
rajesh123rai <rajes1173@gmail.com>
2026-02-03 22:11:13 +0530

    new

---
+++ TheatreSquare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TheatreSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long a = sc.nextLong();
+
+        long tilesAlongN = (n + a - 1) / a;
+        long tilesAlongM = (m + a - 1) / a;
+
+        long result = tilesAlongN * tilesAlongM;
+
+        System.out.println(result);
+        sc.close();
+    }
+}
