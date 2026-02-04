b75e1de4e8673dda8ba0ee16085e8161af1acc86
rajesh123rai <rajes1173@gmail.com>
2026-02-04 23:16:12 +0530

    New

--- Square.java
+++ Square.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
-public class Square
-{
-        public static void main(String[] args)
-        {
+
+public class Square {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < t; i++)
-            {
+        for (int i = 0; i < t; i++) {
             String line = sc.nextLine();
             String[] parts = line.split(" ");
             int a = Integer.parseInt(parts[0]);
             int b = Integer.parseInt(parts[1]);
             int c = Integer.parseInt(parts[2]);
             int d = Integer.parseInt(parts[3]);
-            if(a ==b && b ==c && c==d)
-            {
+            if (a == b && b == c && c == d) {
                 System.out.println("YES");
-            }
-            else
-            {
+            } else {
                 System.out.println("NO");
             }
-            }
-             sc.close();
         }
+        sc.close();
     }
+}
