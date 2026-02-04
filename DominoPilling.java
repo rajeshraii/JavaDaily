d6c5a5556c2003068a718a48c7280b7ec2eea386
rajesh123rai <rajes1173@gmail.com>
2026-02-04 23:12:36 +0530

    New

--- DominoPilling.java
+++ DominoPilling.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class DominoPilling {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println((n * m) /2);
+        System.out.println((n * m) / 2);
         sc.close();
     }
-    
+
 }
