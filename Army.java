9b2aa0aa6d5940652bfccb4e531e44cff5b7e6f9
rajesh123rai <rajes1173@gmail.com>
2026-02-07 10:56:12 +0530

    21

---
+++ Army.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Army{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] d = new int[n-1];
+        for(int i = 0;i < n-1;i++){
+            d[i] = sc.nextInt();
+        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int s =0;
+        for(int i = a-1;i < b-1;i++){
+            s += d[i];
+        }
+        System.out.println(s);
+        sc.close();
+    }
+
+}
