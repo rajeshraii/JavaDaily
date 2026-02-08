44b9b8edb5c0d43d1f19d03bca5311c8929ebf40
rajesh123rai <rajes1173@gmail.com>
2026-02-08 21:24:09 +0530

    08-02-2026

--- Watermelon.java
+++ Watermelon.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
+
 public class Watermelon {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n % 2 == 0 && n!= 2){
+        if (n % 2 == 0 && n != 2) {
             System.out.println("YES");
-        }
-        else{
+        } else {
             System.out.println("NO");
         }
         sc.close();
     }
-    
+
 }
