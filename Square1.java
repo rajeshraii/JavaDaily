bc7d6e60161db32b7c37e6c0a0e52173d3c2b3e1
rajesh123rai <rajes1173@gmail.com>
2026-02-10 22:32:06 +0530

    Feb10

--- Square1.java
+++ Square1.java
@@ -4,23 +4,19 @@ public class Square1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < t; i++)
-            {
+        for (int i = 0; i < t; i++) {
             String line = sc.nextLine();
             String[] parts = line.split(" ");
             int l = Integer.parseInt(parts[0]);
             int r = Integer.parseInt(parts[1]);
             int d = Integer.parseInt(parts[2]);
             int u = Integer.parseInt(parts[3]);
-            if(l ==r && r ==d && d==u)
-            {
+            if (l == r && r == d && d == u) {
                 System.out.println("YES");
-            }
-            else
-            {
+            } else {
                 System.out.println("NO");
             }
-            }
-             sc.close();
         }
+        sc.close();
     }
+}
