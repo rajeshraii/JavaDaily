fd4d9b79f81d9bb41310b9a82190f0980420a82e
rajesh123rai <rajes1173@gmail.com>
2026-02-12 19:40:55 +0530

    Feb12

--- SecondOrdStat.java
+++ SecondOrdStat.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-public class SecondOrdStatg {
+public class SecondOrdStat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
