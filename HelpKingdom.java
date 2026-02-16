a19f81ed522fb65b7dfd9e1df2f6ed5b8868f622
rajesh123rai <rajes1173@gmail.com>
2026-02-16 19:37:58 +0530

    Feb 16

---
+++ HelpKingdom.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class HelpKingdom{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+
+        int dotIndex = number.indexOf('.');
+        String integerPart = number.substring(0, dotIndex);
+        String fractionalPart = number.substring(dotIndex + 1);
+
+        char lastDigit = integerPart.charAt(integerPart.length() - 1);
+        if (lastDigit == '9') {
+            System.out.println("GOTO Vasilisa.");
+            return;
+        }
+        if (fractionalPart.charAt(0) < '5') {
+            System.out.println(integerPart);
+        } else {
+            int rounded = Integer.parseInt(integerPart) + 1;
+            System.out.println(rounded);
+        }
+        sc.close();
+    }
+}
