edc5742212f53a01a2c17576ccf7e561ee25ca80
rajesh123rai <rajes1173@gmail.com>
2026-02-23 22:33:13 +0530

    Feb23

--- DubStep.java
+++ DubStep.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
+
 public class DubStep {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
