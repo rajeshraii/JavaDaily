0334acb634e4e410d12ca3a42d54fee8d17aacfa
rajesh123rai <rajes1173@gmail.com>
2026-02-24 20:37:11 +0530

    Feb24

---
+++ DiverseStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class DiverseStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            String s = sc.next();
+            Set<Character> set = new HashSet<>();
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            boolean diverse = true;
+            
+            for (char c : s.toCharArray()) {
+                if (!set.add(c)) {
+                    diverse = false;
+                    break;
+                }
+                min = Math.min(min, c);
+                max = Math.max(max, c);
+            }
+            
+            // If all distinct, check that max - min == length - 1 (contiguous segment)
+            if (diverse && (max - min != s.length() - 1)) {
+                diverse = false;
+            }
+            
+            System.out.println(diverse ? "Yes" : "No");
+        }
+        sc.close();
+    }
+}
