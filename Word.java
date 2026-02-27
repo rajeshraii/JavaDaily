326fc944b78520171947a89e448891ffb89ccdbd
rajesh123rai <rajes1173@gmail.com>
2026-02-27 23:18:20 +0530

    Feb27

--- Word.java
+++ Word.java
@@ -4,13 +4,15 @@ public class Word {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        
+
         int upper = 0, lower = 0;
         for (char c : s.toCharArray()) {
-            if (Character.isUpperCase(c)) upper++;
-            else lower++;
+            if (Character.isUpperCase(c))
+                upper++;
+            else
+                lower++;
         }
-        
+
         if (upper > lower) {
             System.out.println(s.toUpperCase());
         } else {
