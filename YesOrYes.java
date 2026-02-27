030a31503e49b791ee5e56c60ee88b5cc12fd6d1
rajesh123rai <rajes1173@gmail.com>
2026-02-27 23:17:18 +0530

    Feb27

--- YesOrYes.java
+++ YesOrYes.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
-public class YesOrYes{
-    public static void main(String[] args){
+
+public class YesOrYes {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int t = sc.nextInt();
         sc.nextLine();
 
-        for(int i = 0;i< t;i++){
-        String s = sc.nextLine();
-        
-        if(s.equalsIgnoreCase("yes")){
-            System.out.println("Yes");
+        for (int i = 0; i < t; i++) {
+            String s = sc.nextLine();
+
+            if (s.equalsIgnoreCase("yes")) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
         }
-        else{
-            System.out.println("No");
-        }
-    }
         sc.close();
     }
-    
+
 }
