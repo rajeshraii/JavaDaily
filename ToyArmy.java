86002a6f3b3f51e369434526d5abcca117ebf0e6
rajesh123rai <rajes1173@gmail.com>
2026-03-04 21:16:04 +0530

    Mar04

---
+++ ToyArmy.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ToyArmy{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int result = (3 * n) / 2;
+        
+        System.out.println(result);
+        sc.close();
+    }
+}
